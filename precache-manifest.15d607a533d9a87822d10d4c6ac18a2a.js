8ef167a81859b4afb0b2d1b681de53e9f38c7d89
LiuNing <liuning@cqfmbank.com>
2019-04-06 14:10:44 +0800

    Updates

---
+++ precache-manifest.15d607a533d9a87822d10d4c6ac18a2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-antd-admin/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e8d7409d949f5c4398db",
+    "url": "/react-antd-admin/static/js/runtime~main.9ea20855.js"
+  },
+  {
+    "revision": "180a3348bacd87248e06",
+    "url": "/react-antd-admin/static/js/main.25208171.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-antd-admin/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "180a3348bacd87248e06",
+    "url": "/react-antd-admin/static/css/main.5b37b8e2.chunk.css"
+  },
+  {
+    "revision": "354001107ae51c47f9532a71d5ec6bef",
+    "url": "/react-antd-admin/index.html"
+  }
+];
