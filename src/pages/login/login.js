a69366c2bd564fa5c474c4f4b0e996305f406768
liuningchuan <mayrzlmayrzl@outlook.com>
2019-09-14 23:58:58 +0800

    change login page

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -37,64 +37,28 @@ class Login extends Component {
         const { getFieldDecorator } = this.props.form;
         return (
             <div className="container">
-                <div className="mask" />
-                <Row className="row-wrapper">
-                    <Col span={14}>
-                        <div className="intro-container">
-                            <div className="content">
-                                <div className="title">后台管理系统</div>
-                                <p className="description">运营与管控一体化的整合式服务平台</p>
-                                <p className="description">一起解构数字世界，碰撞科技创新思想</p>
-                            </div>
-                        </div>
-                    </Col>
-                    <Col span={10}>
-                        <div className="login-container">
-                            <h4 className="login-text">登录</h4>
-                            <Form onSubmit={this.handleSubmit}>
-                                <FormItem>
-                                    {getFieldDecorator('email', {
-                                        rules: [{
-                                            required: true, message: '请输入你的账号!'
-                                        }]
-                                    })(
-                                        <Input prefix={<Icon type="user" />} placeholder="账号" />
-                                    )}
-                                </FormItem>
-                                <FormItem>
-                                    {getFieldDecorator('password', {
-                                        rules: [{ required: true, message: '请输入你的密码!' }],
-                                    })(
-                                        <Input prefix={<Icon type="lock" />} type="password" placeholder="密码" />
-                                    )}
-                                </FormItem>
-                                <FormItem>
-                                    <Row style={{ display: 'flex' }}>
-                                        {getFieldDecorator('captcha', {
-                                            rules: [{ required: true, message: '请输入验证码!' }],
-                                        })(
-                                            <Input type="text" placeholder="验证码" />
-                                        )}
-                                        <div className="login-captcha">
-                                            <div>
-                                                <VerifyCode/>
-                                            </div>
-                                        </div>
-                                    </Row>
-                                </FormItem>
-                                <FormItem>
-                                    {getFieldDecorator('remember', {
-                                        valuePropName: 'checked',
-                                        initialValue: true,
-                                    })(
-                                        <Checkbox>记住账号</Checkbox>
-                                    )}
-                                </FormItem>
-                                <Button htmlType="submit" type="primary">登录</Button>
-                            </Form>
-                        </div>
-                    </Col>
-                </Row>
+                <div className="login-container">
+                    <h4 className="login-text">登录</h4>
+                    <Form onSubmit={this.handleSubmit}>
+                        <FormItem>
+                            {getFieldDecorator('email', {
+                                rules: [{
+                                    required: true, message: '请输入你的账号!'
+                                }]
+                            })(
+                                <Input prefix={<Icon type="user" />} placeholder="账号" />
+                            )}
+                        </FormItem>
+                        <FormItem>
+                            {getFieldDecorator('password', {
+                                rules: [{ required: true, message: '请输入你的密码!' }],
+                            })(
+                                <Input prefix={<Icon type="lock" />} type="password" placeholder="密码" />
+                            )}
+                        </FormItem>
+                        <Button htmlType="submit" type="primary">登录</Button>
+                    </Form>
+                </div>
                 <div className="footer">
                     <div className="copyright">Copyright © 2017 Ant Design</div>
                 </div>
