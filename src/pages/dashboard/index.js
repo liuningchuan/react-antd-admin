8ed193fb0b10ad159238b8595e019fdc6cef1567
liuningchuan <mayrzlmayrzl@outlook.com>
2019-10-02 23:58:01 +0800

    add BasicColumn

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 
 import BaseLayout from '../../layouts/BaseLayout';
+import Basiccolumn from '../../components/bizcharts/BasicColumn';
 
 class Dashboard extends Component {
     
     render() {
         return (
             <BaseLayout>
-                <div>23333</div>
+                <Basiccolumn/>
             </BaseLayout>
         );
     }
